ce4aae1e2f6b0eb4da0af5db69aa851ff14bf599
Ethan Roberts <ethanroberts9036@live.com>
2016-05-27 17:37:32 +1200

    Initial Commit
    Added pre-done work on website before git intergration.

---
+++ js/main.js
@@ -0,0 +1,99 @@
+var $previewVid1 = $('#previewVid1');
+var $previewVid2 = $('#previewVid2');
+var $previewVid3 = $('#previewVid3');
+var $previewVid4 = $('#previewVid4');
+var $previewVid5 = $('#previewVid5');
+var $previewVid6 = $('#previewVid6');
+var $previewVid7 = $('#previewVid7');
+var $previewVid8 = $('#previewVid8');
+var $previewVid9 = $('#previewVid9');
+
+$previewVid1.on('mouseenter focus', function () {
+	$previewVid1.get(0).volume = 0.1;
+	$previewVid1.get(0).play();
+});
+
+$previewVid1.on('mouseout blur',function () {
+	$previewVid1.get(0).pause();
+	$previewVid1.get(0).currentTime = 0;
+});
+
+$previewVid2.on('mouseenter focus', function () {
+	$previewVid2.get(0).volume = 0.1;
+	$previewVid2.get(0).play();
+});
+
+$previewVid2.on('mouseout blur',function () {
+	$previewVid2.get(0).pause();
+	$previewVid2.get(0).currentTime = 0;
+});
+
+$previewVid3.on('mouseenter focus', function () {
+	$previewVid3.get(0).volume = 0.1;
+	$previewVid3.get(0).play();
+});
+
+$previewVid3.on('mouseout blur',function () {
+	$previewVid3.get(0).pause();
+	$previewVid3.get(0).currentTime = 0;
+});
+
+$previewVid4.on('mouseenter focus', function () {
+	$previewVid4.get(0).volume = 0.1;
+	$previewVid4.get(0).play();
+});
+
+$previewVid4.on('mouseout blur',function () {
+	$previewVid4.get(0).pause();
+	$previewVid4.get(0).currentTime = 0;
+});
+
+$previewVid5.on('mouseenter focus', function () {
+	$previewVid5.get(0).volume = 0.1;
+	$previewVid5.get(0).play();
+});
+
+$previewVid5.on('mouseout blur',function () {
+	$previewVid5.get(0).pause();
+	$previewVid5.get(0).currentTime = 0;
+});
+
+$previewVid6.on('mouseenter focus', function () {
+	$previewVid6.get(0).volume = 0.1;
+	$previewVid6.get(0).play();
+});
+
+$previewVid6.on('mouseout blur',function () {
+	$previewVid6.get(0).pause();
+	$previewVid6.get(0).currentTime = 0;
+});
+
+$previewVid7.on('mouseenter focus', function () {
+	$previewVid7.get(0).volume = 0.1;
+	$previewVid7.get(0).play();
+});
+
+$previewVid7.on('mouseout blur',function () {
+	$previewVid7.get(0).pause();
+	$previewVid7.get(0).currentTime = 0;
+});
+
+$previewVid8.on('mouseenter focus', function () {
+	$previewVid8.get(0).volume = 0.1;
+	$previewVid8.get(0).play();
+});
+
+$previewVid8.on('mouseout blur',function () {
+	$previewVid8.get(0).pause();
+	$previewVid8.get(0).currentTime = 0;
+});
+
+$previewVid9.on('mouseenter focus', function () {
+	$previewVid9.get(0).volume = 0.1;
+	$previewVid9.get(0).play();
+});
+
+$previewVid9.on('mouseout blur',function () {
+	$previewVid9.get(0).pause();
+	$previewVid9.get(0).currentTime = 0;
+});
